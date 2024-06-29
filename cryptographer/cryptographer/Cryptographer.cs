2bfad807b00e99de5be77f17d6f04f0b0a86c092
Dmitriy Sidyakin <sidyakind@gmail.com>
2024-06-29 19:02:35 +0300

    обновлено

---
+++ cryptographer/cryptographer/Cryptographer.cs
@@ -0,0 +1,9 @@
+
+namespace Cryptographer;
+
+public class Cryptographer {
+    public static void Crypt(string pass, string inputFullFileName, string outputFullFileName, bool isEncryption, bool isOutputOverridable)
+    {
+        throw new NotImplementedException();
+    }
+}
